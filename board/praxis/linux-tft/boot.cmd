b00da8f20449b8b61e10ba56298739570c660be2
Khoi Nguyen <k.van.nguyen@praxis-automation.com>
2024-02-15 07:14:11 +0000

    Build kernel 6.1

--- board/praxis/linux-tft/boot.cmd
+++ board/praxis/linux-tft/boot.cmd
@@ -1,7 +1,11 @@
-echo "loadaddr = " ${loadaddr}
-echo "fdt_addr_r = " ${fdt_addr_r}
-echo "kernel_addr_r = " ${kernel_addr_r}
+echo "loading boot vars"
+setenv load_addr $ramdisk_addr_r
+setenv devnum 0
 
-setenv bootargs root=/dev/mmcblk1p2 rw rootfstype=ext4 clkin_hz=(25000000) earlycon clk_ignore_unused earlyprintk console=ttyS2,1500000n8 rootwait
-fatload mmc 0:1 ${loadaddr} image.itb
-bootm ${loadaddr}
+echo "setting boot args"
+setenv bootargs "root=/dev/mmcblk1p2 earlyprintk console=ttyS2,1500000n8 rw rootwait"
+fatload mmc ${devnum}:${distro_bootpart} ${fdt_addr_r} rockchip/rk3568-praxis-tft.dtb
+fatload mmc ${devnum}:${distro_bootpart} ${kernel_addr_r} Image
+
+echo "booting linux ..."
+booti ${kernel_addr_r} - ${fdt_addr_r}
--- board/praxis/linux-tft/post-image.sh
+++ board/praxis/linux-tft/post-image.sh
@@ -1,9 +1,29 @@
 #!/usr/bin/env bash
 set -e
 
-gzip -fk "${BINARIES_DIR}/Image"
-cp board/praxis/linux-tft/linux-tft.its "${BINARIES_DIR}"
-cd "${BINARIES_DIR}"
-./../../../u-boot/tools/mkimage -f linux-tft.its image.itb
-cp ./../build/host-genimage-16/genimage "${BINARIES_DIR}"
-./../../support/scripts/genimage.sh -c ./../../board/praxis/linux-tft/genimage.cfg
+GIT_SOURCE=${CONFIG_DIR}/../
+UBOOT_SOURCE=${GIT_SOURCE}/uboot-rockchip
+RKBIN=${GIT_SOURCE}/rkbin
+BR2_EXTERNAL_PRAXIS_PATH=${GIT_SOURCE}/buildroot
+
+
+# copy uboot variable file over
+cp -a $BR2_EXTERNAL_PRAXIS_PATH/board/praxis/linux-tft/vars.txt $BINARIES_DIR/
+
+# Build uboot
+echo create uboot.img
+cd ${GIT_SOURCE}
+cd uboot-rockchip; ./make.sh rk3568
+cp uboot.img $BINARIES_DIR/u-boot.itb
+
+# Make idbloader.img
+$UBOOT_SOURCE/tools/mkimage -n rk3568 -T rksd -d $RKBIN/bin/rk35/rk3568_ddr_1056MHz_v1.18.bin:$UBOOT_SOURCE/spl/u-boot-spl.bin $BINARIES_DIR/idbloader.img
+
+# Generate the uboot script
+#$HOST_DIR/bin/mkimage -C none -A arm -T script -d $BR2_EXTERNAL_PRAXIS_PATH/board/praxis/linux-tft/boot.cmd $BINARIES_DIR/boot.scr
+
+# Put the device trees into the correct location
+cd ${BINARIES_DIR}
+cp ${BASE_DIR}/build/host-genimage-16/genimage "${BINARIES_DIR}"
+mkdir -p $BINARIES_DIR/rockchip; cp -a $BINARIES_DIR/*.dtb $BINARIES_DIR/rockchip
+$BASE_DIR/../support/scripts/genimage.sh -c $BR2_EXTERNAL_PRAXIS_PATH/board/praxis/linux-tft/genimage.cfg
